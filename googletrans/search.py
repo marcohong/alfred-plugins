00e466422b11e82b686d6cd957924e0d037ec608
Maco <macohong@hotmail.com>
2018-11-01 16:33:19 +0800

    add code

---
+++ googletrans/search.py
@@ -0,0 +1,116 @@
+# -*-   coding: utf-8 -*-
+'''
+由于alfred-workflow 支持Python 2.6 and 2.7，请使用安装python2.7
+
+感谢以下的项目，排名不分先后
+    execjs: https://github.com/doloopwhile/PyExecJS
+    workflow: https://github.com/deanishe/alfred-workflow
+'''
+import sys
+import time
+import urllib
+from workflow import Workflow, web
+from gen_tk import GenTK
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+LANG = {
+    'zh': ('zh-CN', 'en'),
+    'en': ('en', 'zh-CN')
+}
+
+
+def check_contain_chinese(text):
+    for _str in text.decode('utf-8'):
+        if u'\u4e00' <= _str <= u'\u9fff':
+            return True
+    return False
+
+
+def check_max_len(text):
+    if len(text) > 4891:
+        return False
+    return True
+
+
+def get_url(text, lang='zh'):
+    '''
+    :param lang: <str> zh or en
+    '''
+    sl, tl = LANG.get(lang)
+    js = GenTK()
+    tk = js.get_tk(text)
+    text = urllib.quote(text.encode('utf-8', 'replace'))
+    _url = 'https://translate.google.cn/translate_a/single?'\
+        'client=t&sl={sl}&tl={tl}&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld'\
+        '&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&ssel'\
+        '=5&tsel=5&kc=1&tk={tk}&q={text}'.format(
+            sl=sl, tl=tl, tk=tk, text=text)
+    return _url
+
+
+def request_url(url):
+    req = web.get(url)
+    req.raise_for_status()
+    return req.json()
+
+
+def trans_zh_to_en(text):
+    url = get_url(text, 'zh')
+    return request_url(url)
+
+
+def trans_en_to_zh(text):
+    url = get_url(text, 'en')
+    return request_url(url)
+
+
+def google_translate(text):
+    func = trans_zh_to_en if check_contain_chinese(text) else trans_en_to_zh
+    return func(text)
+
+
+def translate_url(text):
+    lang = 'zh-CN'if not check_contain_chinese(text) else 'en'
+    text = urllib.quote(text.encode('utf-8', 'replace'))
+    url = 'https://translate.google.cn/#auto/{lang}/{text}'.format(
+        lang=lang, text=text)
+    return url
+
+
+def process_child(wf, url, datas):
+    for data in datas[1]:
+        if len(data) >= 2:
+            for sub in data[2]:
+                subtitle = '{0}: {1}'.format(data[0], ', '.join(sub[1]))
+                wf.add_item(title=sub[0], subtitle=subtitle,
+                            arg=url, valid=True)
+
+
+def main(wf):
+    if not wf.args:
+        wf.add_item(title=u'请输入要翻译的内容',
+                    subtitle=u'只支持中文--英文，英文--中文翻译', valid=True)
+    else:
+        text = wf.args[0].decode('utf-8', 'replace')
+        if not check_max_len(text):
+            wf.add_item(title=u'翻译内容太长',
+                        subtitle=u'翻译内容不能超过4891个字符', valid=True)
+        else:
+            time.sleep(0.2)
+            data = google_translate(text)
+            url = translate_url(text)
+            if not data:
+                wf.add_item(title=text, subtitle=text, arg=url, valid=True)
+            else:
+                result = data[0][0][0]
+                wf.add_item(title=text, subtitle=result, arg=url, valid=True)
+                if data[1]:
+                    process_child(wf, url, data)
+
+    wf.send_feedback()
+
+if __name__ == '__main__':
+    wf = Workflow()
+    sys.exit(wf.run(main))
---
+++ ip2region/search.py
@@ -0,0 +1,54 @@
+# -*-   coding: utf-8 -*-
+'''
+由于alfred-workflow 支持Python 2.6 and 2.7，请使用安装python2.7
+
+感谢以下的项目，排名不分先后
+    ip2region: https://github.com/lionsoul2014/ip2region
+    workflow: https://github.com/deanishe/alfred-workflow
+'''
+import os
+import sys
+import socket
+from workflow import Workflow
+from ip2Region import Ip2Region
+
+
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+
+def init_db():
+    file_path = os.path.join(ROOT_PATH, 'ip2region.db')
+    return Ip2Region(file_path)
+
+
+def get_local_ip():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect(('8.8.8.8', 80))
+        ip = sock.getsockname()[0]
+    except:
+        ip = '127.0.0.1'
+    finally:
+        sock.close()
+    return ip
+
+
+def main(wf):
+    if wf.args and Ip2Region.isip(wf.args[0]):
+        region = init_db()
+        data = region.binarySearch(wf.args[0])
+        if data:
+            wf.add_item(title=wf.args[0], subtitle=data[
+                        'region'].decode('utf-8'), valid=True)
+        else:
+            wf.add_item(
+                title=wf.args[0], subtitle=u'Ip地址格式不正确', valid=True)
+    else:
+        ipaddr = get_local_ip()
+        wf.add_item(title=u'本机地址: %s' % ipaddr, valid=True)
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    wf = Workflow()
+    sys.exit(wf.run(main))
